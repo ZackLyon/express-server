f060d2c2001205c80e03cad8a815c78ca4754131
Zack Lyon <zack.j.lyon@gmail.com>
2021-11-03 21:00:25 -0700

    Add getById route and passing test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Order = require('../lib/models/Order');
 
 jest.mock('twilio', () => () => ({
   messages: {
@@ -9,12 +10,12 @@ jest.mock('twilio', () => () => ({
   },
 }));
 
-describe('03_separation-of-concerns-demo routes', () => {
+describe('routes tests', () => {
   beforeEach(() => {
     return setup(pool);
   });
 
-  it.skip('creates a new order in our database and sends a text message', () => {
+  it('creates a new order in our database and sends a text message', () => {
     return request(app)
       .post('/api/v1/orders')
       .send({ quantity: 10 })
@@ -26,5 +27,18 @@ describe('03_separation-of-concerns-demo routes', () => {
         });
       });
   });
+
+  it('gets an order by id', async () => {
+    await Order.insert(99);
+
+    return request(app)
+      .get('/api/v1/orders/1')
+      .then((res) => {
+        // expect(createMessage).toHaveBeenCalledTimes(1);
+        expect(res.body).toEqual({
+          id: '1',
+          quantity: 99,
+        });
+      });
+  });
 });
-//remove .skip
